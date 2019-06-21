cbea0cb85bbddc36208eccb50792b486ed0a1c98
horsicq <horsicq@gmail.com>
2019-06-21 17:47:45 +0200

    Fix

---
+++ gui_source/modevalidator.cpp
@@ -0,0 +1,54 @@
+// Copyright (c) 2019 hors<horsicq@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+#include "modevalidator.h"
+
+ModeValidator::ModeValidator(QObject *parent) : QValidator(parent)
+{
+    data={};
+}
+
+void ModeValidator::setData(ModeValidator::DATA data)
+{
+    this->data=data;
+}
+
+QValidator::State ModeValidator::validate(QString &input, int &pos) const
+{
+    QValidator::State result=Acceptable;
+
+    if(!input.isEmpty())
+    {
+        result=Invalid;
+
+        if(data.mode==MODE_HEX)
+        {
+            bool bSuccess=false;
+            quint64 nValue=input.toULongLong(&bSuccess,16);
+
+            if(bSuccess)
+            {
+                result=Acceptable;
+            }
+        }
+    }
+
+    return result;
+}
---
+++ gui_source/modevalidator.h
@@ -0,0 +1,49 @@
+// Copyright (c) 2019 hors<horsicq@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+#ifndef MODEVALIDATOR_H
+#define MODEVALIDATOR_H
+
+#include <QObject>
+#include <QValidator>
+
+class ModeValidator : public QValidator
+{
+    Q_OBJECT
+public:
+    enum MODE
+    {
+        MODE_HEX=0,
+        MODE_SIGNED=0,
+        MODE_UNSIGNED=0,
+    };
+    struct DATA
+    {
+        MODE mode;
+    };
+
+    explicit ModeValidator(QObject *parent=nullptr);
+    void setData(DATA data);
+    virtual State validate(QString &input,int &pos) const;
+private:
+    DATA data;
+};
+
+#endif // MODEVALIDATOR_H
