0c34fb51085075f5c5f460a4824caa360eab1370
horsicq <horsicq@gmail.com>
2019-06-26 12:53:47 +0200

    Fix

--- asm/win64_msvc.asm
+++ asm/win64_msvc.asm
@@ -38,17 +38,78 @@ __LEAVE     MACRO
             ENDM
 
 _TEXT       SEGMENT
-op_add_64   PROC 
+;################################
+; ADD
+;################################
+op_add      PROC
             __ENTER
             add         rax,rcx
             __LEAVE 
-op_add_64   ENDP 
-
-op_inc_64   PROC 
+op_add      ENDP
+;################################
+; SUB
+;################################
+op_sub      PROC
+            __ENTER
+            sub         rax,rcx
+            __LEAVE
+op_sub      ENDP
+;################################
+; ADC
+;################################
+op_adc      PROC
+            __ENTER
+            adc         rax,rcx
+            __LEAVE
+op_adc      ENDP
+;################################
+; SBB
+;################################
+op_sbb      PROC
+            __ENTER
+            sbb         rax,rcx
+            __LEAVE
+op_sbb      ENDP
+;################################
+; AND
+;################################
+op_and      PROC
+            __ENTER
+            and         rax,rcx
+            __LEAVE
+op_and      ENDP
+;################################
+; XOR
+;################################
+op_xor      PROC
+            __ENTER
+            xor         rax,rcx
+            __LEAVE
+op_xor      ENDP
+;################################
+; OR
+;################################
+op_or       PROC
+            __ENTER
+            or          rax,rcx
+            __LEAVE
+op_or       ENDP
+;################################
+; INC
+;################################
+op_inc      PROC
             __ENTER
             inc         rax
             __LEAVE 
-op_inc_64   ENDP 
+op_inc      ENDP
+;################################
+; DEC
+;################################
+op_dec      PROC
+            __ENTER
+            dec         rax
+            __LEAVE
+op_dec      ENDP
 _TEXT       ENDS
 
 END
