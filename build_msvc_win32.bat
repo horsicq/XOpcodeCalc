2be6f4037c225bc0c756f504d36b57b501e57783
horsicq <horsicq@gmail.com>
2021-07-19 13:08:48 +0200

    Fix: 2021-07-19

--- build_msvc_win32.bat
+++ build_msvc_win32.bat
@@ -3,7 +3,7 @@ set QMAKE_PATH="C:\Qt\5.15.2\msvc2019\bin\qmake.exe"
 set SEVENZIP_PATH="C:\Program Files\7-Zip\7z.exe"
 
 set X_SOURCE_PATH=%~dp0
-set X_BUILD_NAME=xopcode_win32_portable
+set X_BUILD_NAME=xopcodecalc_win32_portable
 set /p X_RELEASE_VERSION=<%X_SOURCE_PATH%\release_version.txt
 
 call %X_SOURCE_PATH%\build_win_generic.cmd
--- build_msvc_win64.bat
+++ build_msvc_win64.bat
@@ -3,7 +3,7 @@ set QMAKE_PATH="C:\Qt\5.15.2\msvc2019_64\bin\qmake.exe"
 set SEVENZIP_PATH="C:\Program Files\7-Zip\7z.exe"
 
 set X_SOURCE_PATH=%~dp0
-set X_BUILD_NAME=xopcode_win64_portable
+set X_BUILD_NAME=xopcodecalc_win64_portable
 set /p X_RELEASE_VERSION=<%X_SOURCE_PATH%\release_version.txt
 
 call %X_SOURCE_PATH%\build_win_generic.cmd
