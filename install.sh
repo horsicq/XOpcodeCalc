29b657ab7dd0e6d9c7142e4da3ada2b07d5b2f9f
horsicq <horsicq@gmail.com>
2021-12-20 08:38:54 +0100

    Fix: 2021-12-20

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 export X_SOURCE_PATH=$PWD
 
 cp -f $X_SOURCE_PATH/build/release/xocalc                            /usr/bin/
-cp -f $X_SOURCE_PATH/DEBIAN/xocalc.desktop                           /usr/share/applications/
-cp -Rf $X_SOURCE_PATH/DEBIAN/hicolor/                               /usr/share/icons/
+cp -f $X_SOURCE_PATH/LINUX/xocalc.desktop                           /usr/share/applications/
+cp -Rf $X_SOURCE_PATH/LINUX/hicolor/                               /usr/share/icons/
