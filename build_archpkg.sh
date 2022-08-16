b7a8afc124f99d166005807476b8f72562adbf4b
horsicq <horsicq@gmail.com>
2022-08-16 23:09:23 +0200

    Fix: 2022-08-16

--- build_archpkg.sh
+++ build_archpkg.sh
@@ -2,8 +2,8 @@
 mkdir release
 cp -f PKGBUILD release/
 cd release
+env EUID=1 makepkg -Acs --noconfirm
 #makepkg -Acs
-source PKGBUILD && pacman -Syu --noconfirm --needed --asdeps "${makedepends[@]}" "${depends[@]}"
 rm -Rf PKGBUILD
 rm -Rf XOpcodeCalc
 cd ..
